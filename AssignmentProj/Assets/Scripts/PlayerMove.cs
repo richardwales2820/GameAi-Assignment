3d846ef9a5206a031692b2f73110466cb38da7fa
Richard Wales <richard_wales@knights.ucf.edu>
2016-01-29 01:38:00 -0500

    Added rays for wall sensors
    Added the lines for the raycasting. Actual raycasting still to be
    added. Rays turn with player and have set length.

--- AssignmentProj/Assets/Scripts/PlayerMove.cs
+++ AssignmentProj/Assets/Scripts/PlayerMove.cs
@@ -5,33 +5,37 @@ public class PlayerMove : MonoBehaviour {
 
     public float playerSpeed = 1;
     public float playerRotationSpeed = 45;
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		//Gets the current heading of the player
         float currentAngle = transform.eulerAngles.z;
 
-        Debug.Log("Sin: " + Mathf.Sin(Mathf.Deg2Rad * currentAngle) + " *** Cos: " + Mathf.Cos(Mathf.Deg2Rad * currentAngle));
-
+		//While W is pressed, the X and Y coordinates of the player are incremented with trig
+		//such that they will be moved forward in their current heading
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = new Vector3(transform.position.x + playerSpeed * -Mathf.Sin(Mathf.Deg2Rad * currentAngle), 
                 transform.position.y + playerSpeed * Mathf.Cos(Mathf.Deg2Rad * currentAngle), 0);
         }
 
+		//While A is pressed, the player rotates to the left with a specified speed
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, 0, playerRotationSpeed);
         }
 
+		//While D is pressed, rotate to the right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 0, -playerRotationSpeed);
         }
 
+		//Same as the "w" key code but with opposite directions
         if (Input.GetKey(KeyCode.S))
         {
             transform.position = new Vector3(transform.position.x - playerSpeed * -Mathf.Sin(Mathf.Deg2Rad * currentAngle),
