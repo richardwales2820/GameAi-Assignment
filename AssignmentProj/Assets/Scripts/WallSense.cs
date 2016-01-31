2f6a4ada76c21211a837a5856a0319d3ace7a12e
twhitcomb <ty_n_w@yahoo.com>
2016-01-31 17:28:46 -0500

    Wall Sensing Actually Works
    I made each object (aside from the player object) have the Component
    "Physics2D > Collidable Box", then used the Physics2D.Raycast function.
    If that function detects something, the line which detected it turns
    purple.

--- AssignmentProj/Assets/Scripts/WallSense.cs
+++ AssignmentProj/Assets/Scripts/WallSense.cs
@@ -3,73 +3,54 @@ using System.Collections;
 
 public class WallSense : MonoBehaviour {
 
-	public float floatHeight;
-	public float liftForce;
-	public float damping;
-	public Rigidbody2D rb2D;
-	public float rayDistance = 10;
+    // Ray Distance
+	public float rayDistance = 5;
+
+    // Color of GUI rays (turns a different color when it collides with a collider)
+    public Color forwardLineColor, rightLineColor, leftLineColor;
 
 	// Use this for initialization
 	void Start () {
-		rb2D = GetComponent<Rigidbody2D>();
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		
 	}
 
 	void FixedUpdate() {
+
 		//Gets the angle of the player
 		float currentAngle = transform.eulerAngles.z;
 
 		//Initialize vector positions of the rays
 		//Uses the same trig as the movement code to cast ray in front of player
 		//Left and Right rays have their angles shifted by +- 90 degrees
-		Vector3 forwardRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * currentAngle), 
-			Mathf.Cos(Mathf.Deg2Rad * currentAngle), transform.position.z);
-		Vector3 rightRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * (currentAngle - 90)), 
-			Mathf.Cos(Mathf.Deg2Rad * (currentAngle - 90)), transform.position.z);
-		Vector3 leftRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * (currentAngle + 90)), 
-			Mathf.Cos(Mathf.Deg2Rad * (currentAngle + 90)), transform.position.z);
+		Vector3 forwardRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * currentAngle), Mathf.Cos(Mathf.Deg2Rad * currentAngle), transform.position.z);
+		Vector3 rightRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * (currentAngle - 90)), Mathf.Cos(Mathf.Deg2Rad * (currentAngle - 90)), transform.position.z);
+		Vector3 leftRayPos = new Vector3(-Mathf.Sin(Mathf.Deg2Rad * (currentAngle + 90)), Mathf.Cos(Mathf.Deg2Rad * (currentAngle + 90)), transform.position.z);
 
 		//Normalize vectors to only have directions
-		forwardRayPos.Normalize ();
-		rightRayPos.Normalize ();
-		leftRayPos.Normalize ();
+		forwardRayPos.Normalize();
+		rightRayPos.Normalize();
+		leftRayPos.Normalize();
 
-        /*
-		// This section will host the actual raycasting to check for object detection
-		RaycastHit2D hitForward = Physics2D.Raycast(transform.position, transform.position + 
-																rayDistance * forwardRayPos);
+        // Initialize colors to white
+        forwardLineColor = Color.white;
+        rightLineColor = Color.white;
+        leftLineColor = Color.white;
 
-		if (hitForward.collider != null) {
-			float distance = Mathf.Abs(hitForward.point.y - transform.position.y);
-			//float heightError = floatHeight - distance;
-			//float force = liftForce * heightError - rb2D.velocity.y * damping;
-			//rb2D.AddForce(Vector3.up * force);
-
-			if (distance < 5)
-				Debug.Log ("Hit");
-		}
-        */
-
-        object[] obj = GameObject.FindSceneObjectsOfType(typeof(GameObject));
-
-        foreach (object o in obj) {
-
-            GameObject g = (GameObject)o;
-            //Debug.Log(g.name);
-
-            //Mesh.bounds
-            
-        }
+        //Check if the rays are colliding with an object; if they are, draw them magenta
+        if (Physics2D.Raycast(transform.position, forwardRayPos, rayDistance).collider != null) forwardLineColor = Color.magenta;
+        if (Physics2D.Raycast(transform.position, rightRayPos, rayDistance).collider != null) rightLineColor = Color.magenta;
+        if (Physics2D.Raycast(transform.position, leftRayPos, rayDistance).collider != null) leftLineColor = Color.magenta;
 
         //Draw lines that extend from the player in their respective directions with a set length
-        Debug.DrawLine (transform.position, transform.position + rayDistance * forwardRayPos);
-		Debug.DrawLine (transform.position, transform.position + rayDistance * rightRayPos);
-		Debug.DrawLine (transform.position, transform.position + rayDistance * leftRayPos);
+        Debug.DrawLine(transform.position, transform.position + rayDistance * forwardRayPos, forwardLineColor);
+		Debug.DrawLine(transform.position, transform.position + rayDistance * rightRayPos, rightLineColor);
+		Debug.DrawLine(transform.position, transform.position + rayDistance * leftRayPos, leftLineColor);
 
-		Debug.ClearDeveloperConsole ();
+        Debug.ClearDeveloperConsole();
 	}
 }
