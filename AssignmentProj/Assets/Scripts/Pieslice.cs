48a59c3f86b40c3c178f6c499e58929b8e619d73
Richard Wales <richard_wales@knights.ucf.edu>
2016-02-01 12:09:55 -0500

    Added pie slices

---
+++ AssignmentProj/Assets/Scripts/Pieslice.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pieslice : MonoBehaviour {
+
+	public Text sliceText;
+	int agents = 0;
+	//private bool agent1In, agent2In;
+
+	// Use this for initialization
+	void Start () {
+		agents = 0;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		agents++;
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		agents--;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.Log (agents);
+
+		sliceText.text = sliceText.tag + " level " + agents;
+	}
+}
